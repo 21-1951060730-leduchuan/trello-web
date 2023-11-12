896b60e3135ec0fc092d1f777639d5c9cf5f3c48
Huanle01 <huanle120201@gmail.com>
2023-11-12 20:08:05 +0700

    dark light mode system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
-import DeleteIcon from "@mui/icons-material/Delete";
-import AlarmIcon from "@mui/icons-material/Alarm";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Box from '@mui/material/Box';
 import { useColorScheme } from "@mui/material/styles";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
 function ModeToogle() {
   const { mode, setMode } = useColorScheme();
   return (
@@ -17,36 +20,51 @@ function ModeToogle() {
     </Button>
   );
 }
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode);
+  };
+  return (
+    <>
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="label-select-dark-light-mode"
+          value={mode}
+          onChange={handleChange}
+          autoWidth
+          label="Mode"
+        >
+          <MenuItem value="light">
+            <Box style={{ display: "flex", alignItems: "center",gap:1 }}>
+              <LightModeIcon fontSize="small"/> Light
+            </Box>
+          </MenuItem>{" "}
+          <MenuItem value="dark">
+            <Box style={{ display: "flex", alignItems: "center",gap:1 }}>
+              <DarkModeOutlinedIcon fontSize="small"/> Dark
+            </Box>
+          </MenuItem>{" "}
+          <MenuItem value="system">
+            <Box style={{ display: "flex", alignItems: "center",gap:1 }}>
+              <SettingsBrightnessIcon fontSize="small"/> System
+            </Box>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  );
+}
 function App() {
   return (
     <>
       <ModeToogle />
       <hr />
-      <Button variant="contained">Hello world</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <Button color="secondary">Secondary</Button>
-      <Button variant="contained" color="success">
-        Success
-      </Button>
-      <Button variant="outlined" color="error">
-        Error
-      </Button>
-      <Stack direction="row" spacing={1}>
-        <IconButton aria-label="delete">
-          <DeleteIcon />
-        </IconButton>
-        <IconButton aria-label="delete" disabled color="primary">
-          <DeleteIcon />
-        </IconButton>
-        <IconButton color="secondary" aria-label="add an alarm">
-          <AlarmIcon />
-        </IconButton>
-        <IconButton color="primary" aria-label="add to shopping cart">
-          <AddShoppingCartIcon />
-        </IconButton>
-      </Stack>
+      <ModeSelect />
     </>
   );
 }
