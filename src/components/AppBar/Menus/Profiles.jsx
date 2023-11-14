bd04306800c942c4f2e603d82ceae905ae5d8b11
Huanle01 <huanle120201@gmail.com>
2023-11-14 23:05:27 +0700

    header#1

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+function Profile() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? "basic-menu-profiles" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+        >
+          <Avatar
+            sx={{ width: "30px", height: "30px" }}
+            alt="Huấn Lê"
+            src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/373627810_271341892418607_8970231156656147667_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=bldHRvZu03MAX8vTvr5&_nc_ht=scontent.fhan17-1.fna&oh=00_AfCKPcNXukyGfwkk-AWEUffa3XWXyMPptf2VRCKibdQH4A&oe=65589CE1"
+          />
+        </IconButton>
+      </Tooltip>
+
+      <Menu
+        id="basic-menu-profiles"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button-profiles",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width: "28px", height: "28px", mr: 2 }} /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width: "28px", height: "28px", mr: 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+}
+
+export default Profile;
