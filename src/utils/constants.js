59ed02faec959abb157ea5bc1fb7c1b87f9e223d
Huanle01 <huanle120201@gmail.com>
2023-12-13 22:46:39 +0700

    fix build_mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,9 @@
-export const API_ROOT='https://trello-api-jty2.onrender.com'
-// export const API_ROOT='http://localhost:8017'
+let apiRoot = "";
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8017";
+}
+if (process.env.BUILD_MODE === "prod") {
+  apiRoot = "https://trello-api-jty2.onrender.com";
+}
+// export const API_ROOT='https://trello-api-jty2.onrender.com'
+export const API_ROOT = apiRoot;
